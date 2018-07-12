6748892b640db282776360dfafa6d506a7145f12
pacificbyte <34432840+pacificbyte@users.noreply.github.com>
2018-07-11 21:02:21 -0700

    Next commit with JUnit 4

---
+++ src/main/java/code/test/IntegersComparator.java
@@ -0,0 +1,56 @@
+package code.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class IntegersComparator {
+
+    public Integer[] generateIntegersArray(int size, int min, int max) {
+        Integer[] integers = new Integer[size];
+        
+        for(int i = 0; i < integers.length; i++) {
+            integers[i] = ThreadLocalRandom.current().nextInt(min, max + 1);
+        }
+        
+        return integers;
+    }
+    
+    public Boolean checkDuplicates(Integer[] integers) {
+        Set<Integer> integersSet = new HashSet<Integer>(Arrays.asList(integers));
+        if(integersSet.size() != integers.length) {
+            System.out.println("There are duplicates!");
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public void printDuplicates(Integer[] integers) {
+        System.out.println("The repeating elements are : ");
+    
+        for (int i = 0; i < integers.length; i++) {
+            if (integers[Math.abs(integers[i])] >= 0) {
+                integers[Math.abs(integers[i])] = -integers[Math.abs(integers[i])];
+            }
+            else {
+                System.out.println(Math.abs(integers[i]));
+            }
+        }         
+    }
+    
+    public void printIntersectedIntegers(Integer[] integers1, Integer[] integers2) {
+        Set<Integer> integersSet1 = new HashSet<Integer>(Arrays.asList(integers1));
+        Set<Integer> integersSet2 = new HashSet<Integer>(Arrays.asList(integers2));
+        integersSet1.retainAll(integersSet2);
+
+        List sortedList = new ArrayList(integersSet1);
+        Collections.sort(sortedList);
+        
+        System.out.println(sortedList.toString());
+    }
+}
---
+++ src/test/java/code/test/IntegersComparatorTest.java
@@ -0,0 +1,60 @@
+package code.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class IntegersComparatorTest
+{
+    IntegersComparator iComparator;
+    Integer[] integers;
+    
+    @Before
+    public void init() {
+        iComparator = new IntegersComparator();
+    }
+
+    @Test
+    public void generateIntegersArray_1000() {
+        int expectedSize = 1000;
+        integers = iComparator.generateIntegersArray(1000, 1, 1000);
+        assertEquals(expectedSize, integers.length);
+    }
+    
+    @Test
+    public void checkDuplicates_true() {
+        integers = iComparator.generateIntegersArray(1000, 1, 1000);
+        Boolean isThereDuplicates = iComparator.checkDuplicates(integers);
+        assertTrue(isThereDuplicates);
+    }
+    
+    @Test
+    public void checkDuplicates_false() {
+        integers = iComparator.generateIntegersArray(1, 1, 1000);
+        Boolean isThereDuplicates = iComparator.checkDuplicates(integers);
+        assertFalse(isThereDuplicates);
+    }
+    
+    @Test
+    public void printDuplicates() {
+        integers = iComparator.generateIntegersArray(1000, 1, 1000);
+        iComparator.printDuplicates(integers);
+        
+        //not really sure what to assert here!
+    }
+    
+    @Test
+    public void printIntersectedIntegers() {
+        integers = iComparator.generateIntegersArray(1000, 1, 1000);
+        Integer[] integers2 = iComparator.generateIntegersArray(1000, 1, 1000);
+        iComparator.printIntersectedIntegers(integers, integers2);
+        
+        //not really sure what to assert here!
+    }
+}
